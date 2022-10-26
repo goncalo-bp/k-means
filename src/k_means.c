d92521b1447dc397be5c8c4e9f22fe85eefed62c
Gonçalo Pinto <blanc@blanc-mac.Home>
2022-10-26 18:28:27 +0100

    variaveis globais

--- src/k_means.c
+++ src/k_means.c
@@ -10,124 +10,112 @@ typedef struct point {
     float x;
     float y;
     int cluster;
-    float minDist;
 } Point;
 
 typedef struct cluster {
     float x;
     float y;
-    int nPoints;
 } Cluster;
 
-Point *new_point(float x, float y) {  
-    Point *p = malloc(sizeof(struct point));
-    p->x = x;
-    p->y = y;
-    p->cluster = -1;
-    p->minDist = TMP_MAX;
-    return p;
-}
+Point *points;
+Cluster *centroids;
+Cluster *old_centroids;
+int *n_points;
 
-Cluster *new_cluster(float x, float y) {
-    Cluster *c = malloc(sizeof(struct cluster));
-    c->x = x;
-    c->y = y;
-    c->nPoints = 0;
-    return c;
+void aloca() {
+    points = malloc(sizeof(struct point) * N);
+    centroids = malloc(sizeof(struct cluster) * K);
+    old_centroids = malloc(sizeof(struct cluster) * K);
+    n_points = malloc(sizeof(int) * K);
 }
 
 float distance(float x1, float y1, float x2, float y2) {
     return (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2);
 }
 
-void inicializa(Point **points, Cluster **clusters) {
+void inicializa() {
     srand(10);
     for(int i = 0; i < N; i++) {
-        float x = (float) rand() / RAND_MAX;
-        float y = (float) rand() / RAND_MAX;
-        points[i] = new_point(x, y);
+        points[i].x = (float) rand() / RAND_MAX;
+        points[i].y = (float) rand() / RAND_MAX;
     }
     for(int i = 0; i < K; i++) {
-        float x = points[i]->x;
-        float y = points[i]->y;
-        clusters[i] = new_cluster(x, y);
+        centroids[i].x = points[i].x;
+        centroids[i].y = points[i].y;
     }
 }
 
-void set_distance(Point **points, Cluster **clusters) { // OTIMIZAR (Ciclo dentro do outro)
+void cluster_points() { // OTIMIZAR (Ciclo dentro do outro)
     for (int j = 0; j < N; j++) {
-        float dist = distance(points[j]->x, points[j]->y , clusters[0]->x, clusters[0]->y);
-        points[j]->minDist = dist;
-        points[j]->cluster = 0;
+        float dist = distance(points[j].x, points[j].y , centroids[0].x, centroids[0].y);
+        points[j].cluster = 0;
         for (int i = 1; i < K; i++) {
-            dist = distance(points[j]->x, points[j]->y , clusters[i]->x, clusters[i]->y);
-            if (dist < points[j]->minDist) {
-                points[j]->cluster = i;
-                points[j]->minDist = dist;
+            float tmp = distance(points[j].x, points[j].y , centroids[i].x, centroids[i].y);
+            if (tmp < dist) {
+                points[j].cluster = i;
+                dist = tmp;
             }
         }
-        clusters[points[j]->cluster]->nPoints += 1;
     }
 }
 
-void semnome(Point **points, Cluster **clusters) {
-    float sumX[K], sumY[K];
+void reevaluate_points() {
+    float sum_x[K], sum_y[K];
+
     for (int i = 0; i < K; i++) {
-        sumX[i] = 0;
-        sumY[i] = 0;
+        sum_x[i] = 0;
+        sum_y[i] = 0;
+        n_points[i] = 0;
     }
 
     for (int i = 0; i < N; i++) {
-        sumX[points[i]->cluster] += points[i]->x;
-        sumY[points[i]->cluster] += points[i]->y;
-
-        points[i]->minDist = TMP_MAX;
+        sum_x[points[i].cluster] += points[i].x;
+        sum_y[points[i].cluster] += points[i].y;
+        n_points[points[i].cluster] += 1;
     }
 
     for (int i = 0; i < K; i++) {
-        clusters[i]->x = sumX[i] / clusters[i]->nPoints;
-        clusters[i]->y = sumY[i] / clusters[i]->nPoints;
+        centroids[i].x = sum_x[i] / n_points[i];
+        centroids[i].y = sum_y[i] / n_points[i];
     }
 }
 
-int has_converged(Cluster **clusters, Cluster **oldClusters) {
+int has_converged() {
     for (int i = 0; i < K; i++)
-        if (clusters[i]->x != oldClusters[i]->x || clusters[i]->y != oldClusters[i]->y)
+        if (centroids[i].x != old_centroids[i].x || centroids[i].y != old_centroids[i].y)
             return 0;
     return 1;
 }
 
-void copy_clusters(Cluster **clusters, Cluster **oldClusters) {
+void copy_centroids() {
     for (int i = 0; i < K; i++) {
-        oldClusters[i] = new_cluster(clusters[i]->x, clusters[i]->y);
-        clusters[i]->nPoints = 0;
+        old_centroids[i].x = centroids[i].x;
+        old_centroids[i].y = centroids[i].y;
     }
 }
 
 int main() {
     int iterations = -1;
 
-    Point **points = malloc(sizeof(Point *) * N);
-    Cluster **clusters = malloc(sizeof(Cluster *) * K);
-    Cluster **oldClusters = malloc(sizeof(Cluster *) * K);
+    aloca();
 
-    inicializa(points, clusters);
+    inicializa();
 
-    while (iterations == -1 || !has_converged(clusters, oldClusters)) {
-        copy_clusters(clusters, oldClusters);
-        set_distance(points, clusters);
-        semnome(points, clusters);
+    while (iterations == -1 || !has_converged()) {
+        copy_centroids();
+        cluster_points();
+        reevaluate_points();
         iterations++;
     }
 
     printf("N = %d, K = %d\n", N, K);
     for (int i = 0; i < K; i++)
-        printf("Center: (%.3f, %.3f) : Size: %d\n", clusters[i]->x, clusters[i]->y, clusters[i]->nPoints);
+        printf("Center: (%.3f, %.3f) : Size: %d\n", centroids[i].x, centroids[i].y, n_points[i]);
     printf("Iterations: %d\n", iterations);
 
     free(points);
-    free(clusters);
-    free(oldClusters);
+    free(centroids);
+    free(old_centroids);
 
     return 0;
 }
