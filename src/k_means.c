6771734878beb0cd7d78a88851b69630bf3cb959
TBlanc157 <gmpinto11@gmail.com>
2022-11-16 17:57:55 +0000

    par

--- src/k_means.c
+++ src/k_means.c
@@ -40,9 +40,14 @@ void inicializa() {
     }
 }
 
+// Calcula a distância entre dois pontos
+float distance(Point p1, Point p2) {
+    return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
+}
+
 // Calcula as distâncias dos pontos aos clusters, associa-os ao cluster mais próximo e reavalia as coordenadas dos clusters
-void set_clusters() { 
-    int cluster_id;
+void cluster_points() { 
+    #pragma omp parallel for
     for (int i = 0; i < K; i++) {
         new_x[i] = 0; 
         new_y[i] = 0; 
@@ -50,33 +55,57 @@ void set_clusters() {
     }
 
     // Cálculo das distancias dos pontos aos clusters e associação ao cluster mais próximo
-    for (int j = 0; j < N; j++) {
-        float dist = (points[j].x - centroids[0].x)*(points[j].x - centroids[0].x) + (points[j].y - centroids[0].y)*(points[j].y - centroids[0].y);
-        cluster_id = 0;
-        for (int i = 1; i < K; i++) {        
-            float tmp = (points[j].x - centroids[i].x)*(points[j].x - centroids[i].x) + (points[j].y - centroids[i].y)*(points[j].y - centroids[i].y);
-            if (tmp < dist) {
-                cluster_id = i;
-                dist = tmp;
+    #pragma omp parallel
+    {
+        #pragma omp for schedule(static) reduction(+ : new_x[:K], new_y[:K], n_points[:K])
+        for (int j = 0; j < N; j++) {
+            float dist = distance(points[j], centroids[0]);
+            int cluster_id = 0;
+            for (int i = 1; i < K; i++) {        
+                float tmp = distance(points[j], centroids[i]);
+                if (tmp < dist) {
+                    cluster_id = i;
+                    dist = tmp;
+                }
             }
+
+            n_points[cluster_id]++;
+            new_x[cluster_id] += points[j].x;
+            new_y[cluster_id] += points[j].y;
+        }
+    }
+    
+    /*
+    #pragma omp parallel for schedule(static)
+    for (int i = 0; i < N; i++) {
+        float dists[K];
+        #pragma omp parallel for schedule(static)
+        for (int j = 0; j < K; j++) {
+            dists[j] = distance(points[i], centroids[j]);
         }
 
-        n_points[cluster_id] += 1;
-        new_x[cluster_id] += points[j].x;
-        new_y[cluster_id] += points[j].y;
+        float min_dist = dists[0];
+        int cluster = 0;
+        for (int j = 1; j < K; j++) {
+            if (dists[j] < min_dist) {
+                cluster = j;
+                float min_dist = dists[j];
+            }
+        }
+        
+        #pragma omp atomic
+        new_x[cluster] += points[i].x;
+        #pragma omp atomic
+        new_y[cluster] += points[i].y;
+        #pragma omp atomic
+        n_points[cluster]++;
     }
-}
-
-void calculate_clusters() {
-    // Reavaliação e possível reatribuição das coordenadas dos centroides
-    #pragma omp simd
-    for (int i = 0; i < K; i++) {
-        new_x[i] = new_x[i] / n_points[i];
-        new_y[i] = new_y[i] / n_points[i];
-
-        centroids[i].x = new_x[i];
-        centroids[i].y = new_y[i];
-    }
+    */
+   #pragma omp parallel for
+        for (int i = 0; i < K; i++) {
+            centroids[i].x = new_x[i] / n_points[i];;
+            centroids[i].y = new_y[i] / n_points[i];;
+        }
 }
 
 int main(int argc, char **argv) {
@@ -87,6 +116,8 @@ int main(int argc, char **argv) {
     }
     N = atoi(argv[1]);
     K = atoi(argv[2]);
+    if (argc == 4)
+        omp_set_num_threads(atoi(argv[3]));
     
     int iterations = -1;
 
@@ -95,8 +126,7 @@ int main(int argc, char **argv) {
     inicializa();
 
     while (iterations < 20) {
-        set_clusters();
-        calculate_clusters();
+        cluster_points();
         iterations++;
     }
 
@@ -113,3 +143,7 @@ int main(int argc, char **argv) {
 
     return 0;
 }
+
+/*
+qg2PbeK5
+*/
