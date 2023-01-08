741a35f52c4042d9093d87ac55e937b13bbc14b1
TBlanc157 <gmpinto11@gmail.com>
2023-01-08 17:46:28 +0000

    mpi implementation

--- src/k_means.c
+++ src/k_means.c
@@ -1,118 +1,199 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h>
+#include <mpi.h>
+#include <unistd.h>
+#include <math.h>
+#include <errno.h>
+#include <stdarg.h>
 
-typedef struct point {
-    float x;
-    float y;
-} Point;
+#define MAX_ITERATIONS 20
 
-int N;
-int K;
-
-Point *points __attribute__((aligned (32)));
-Point *centroids __attribute__((aligned (32)));
-int *n_points __attribute__((aligned (32)));
-float *new_x __attribute__((aligned (32)));
-float *new_y __attribute__((aligned (32)));
-
-// Aloca espaço para as variáveis globais
-void aloca() {
-    points = malloc(sizeof(struct point) * N);
-    centroids = malloc(sizeof(struct point) * K);
-    n_points = malloc(sizeof(int) * K);
-    new_x = malloc(sizeof(float) * K);
-    new_y = malloc(sizeof(float) * K);
-}
+int nClusters = 0;
+int nPoints = 0;
+int nProcs = 0;
 
 // Inicializa os pontos com valores aleatóris e os centroides com os valores dos primeiros K pontos
-void inicializa() {
+void init(float *data_x, float *data_y, float *k_means_x, float *k_means_y) {
     srand(10);
-    for(int i = 0; i < N; i++) {
-        points[i].x = (float) rand() / RAND_MAX;
-        points[i].y = (float) rand() / RAND_MAX;
+    for(int i = 0; i < nPoints; i++) {
+        data_x[i] = (float) rand() / RAND_MAX;
+        data_y[i] = (float) rand() / RAND_MAX;
     }
-    for(int i = 0; i < K; i++) {
-        centroids[i].x = points[i].x;
-        centroids[i].y = points[i].y;
+    for(int i = 0; i < nClusters; i++) {
+        k_means_x[i] = data_x[i];
+        k_means_y[i] = data_y[i];
     }
 }
 
 // Calcula a distância entre dois pontos
-float distance(Point p1, Point p2) {
-    return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
+float distance(float p1_x, float p1_y, float p2_x, float p2_y) {
+    return (p1_x - p2_x) * (p1_x - p2_x) + (p1_y - p2_y) * (p1_y - p2_y);
 }
 
-// Calcula as distâncias dos pontos aos clusters, associa-os ao cluster mais próximo e reavalia as coordenadas dos clusters
-void cluster_points() { 
-    //#pragma omp parallel for
-    for (int i = 0; i < K; i++) {
-        new_x[i] = 0; 
-        new_y[i] = 0; 
-        n_points[i] = 0; 
-    }
+/* This function goes through that data points and assigns them to a cluster */
+void assign_cluster(float *k_x, float *k_y, float *recv_x, float *recv_y, float *sum_x, float *sum_y, int *k_points) {
+	int index;
+	float min_dist, temp_dist;
 
-    // Cálculo das distancias dos pontos aos clusters e associação ao cluster mais próximo
-    #pragma omp parallel
-    {
-        #pragma omp for schedule(static) reduction(+ : new_x[:K], new_y[:K], n_points[:K])
-        for (int j = 0; j < N; j++) {
-            float dist = distance(points[j], centroids[0]);
-            int cluster_id = 0;
-            for (int i = 1; i < K; i++) {        
-                float tmp = distance(points[j], centroids[i]);
-                if (tmp < dist) {
-                    cluster_id = i;
-                    dist = tmp;
-                }
-            }
+	for(int i = 0; i < nPoints/nProcs; i++) {
+		min_dist = distance(k_x[0], k_y[0], recv_x[i], recv_y[i]);
+		index = 0;
 
-            n_points[cluster_id]++;
-            new_x[cluster_id] += points[j].x;
-            new_y[cluster_id] += points[j].y;
-        }
-    }
-    
-    //#pragma omp parallel for
-    for (int i = 0; i < K; i++) {
-        centroids[i].x = new_x[i] / n_points[i];;
-        centroids[i].y = new_y[i] / n_points[i];;
-    }
+		for(int j = 1; j < nClusters; j++) {
+			temp_dist = distance(k_x[j], k_y[j], recv_x[i], recv_y[i]);
+
+			// new minimum distance found
+			if(temp_dist < min_dist) {
+				min_dist = temp_dist;
+				index = j;
+			}
+		}
+
+		sum_x[index] += recv_x[i];
+		sum_y[index] += recv_y[i];
+		k_points[index]++;
+	}
 }
 
 int main(int argc, char **argv) {
-    if (argc < 3) {
+    // initialize the MPI environment
+	MPI_Init(&argc, &argv);
+
+	// get number of processes
+	int size;
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	nProcs = size;
+
+	// get rank
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // send buffers
+	float *k_means_x = NULL;		// k means corresponding x values
+	float *k_means_y = NULL;		// k means corresponding y values
+
+	int *k_points = NULL;			// number of points assigned to each cluster
+	int *recv_k_points = NULL;
+
+	float *sum_x = NULL;
+	float *sum_y = NULL;
+	float *res_x = NULL;
+	float *res_y = NULL;
+
+	float *data_x_points = NULL;
+	float *data_y_points = NULL;
+	float *recv_x = NULL;
+	float *recv_y = NULL;
+
+
+	if (argc < 2) {
         printf("Número argumentos: %d\n", argc);
-        printf("Argumentos insuficientes: ./k_means <N> <K>\n");
-        return -1;
+        printf("Argumentos insuficientes: ./k_means <Num_Points> <Num_Clusters>\n");
+        exit(-1);
     }
-    N = atoi(argv[1]);
-    K = atoi(argv[2]);
-    if (argc > 3)
-        omp_set_num_threads(atoi(argv[3]));
-        
-    
-    int iterations = -1;
+    nPoints = atoi(argv[1]);
+    nClusters = atoi(argv[2]);
 
-    aloca();
+	// allocate memory for arrays
+	k_means_x = (float *)malloc(sizeof(float) * nClusters);
+	k_means_y = (float *)malloc(sizeof(float) * nClusters);
+	if(k_means_x == NULL || k_means_y == NULL) {
+		perror("malloc");
+		exit(-1);
+	}
 
-    inicializa();
+	// allocate memory for an array of data points
+	data_x_points = (float *)malloc(sizeof(float) * nPoints);
+	data_y_points = (float *)malloc(sizeof(float) * nPoints);
+	if(data_x_points == NULL || data_y_points == NULL) {
+		perror("malloc");
+		exit(-1);
+	}
 
-    while (iterations < 20) {
-        cluster_points();
-        iterations++;
+	if (rank == 0){
+		init(data_x_points, data_y_points, k_means_x, k_means_y);
+
+		printf("\nRunning k-means algorithm for %d iterations...\n\n", MAX_ITERATIONS);
+		for(int i = 0; i < nClusters; i++) {
+			printf("Initial K-means: (%f, %f)\n", k_means_x[i], k_means_y[i]);
+		}
+	}
+
+	// allocate memory for receive buffers
+	recv_x = (float *) malloc(sizeof(float) * (nPoints/nProcs));
+	recv_y = (float *) malloc(sizeof(float) * (nPoints/nProcs));
+	if(recv_x == NULL || recv_y == NULL) {
+		perror("malloc");
+		exit(-1);
+	}
+
+
+	k_points = (int *)malloc(sizeof(int) * nClusters);
+	recv_k_points = (int *)malloc(sizeof(int) * nClusters);
+	sum_x = (float *)malloc(sizeof(float) * nClusters);
+	sum_y = (float *)malloc(sizeof(float) * nClusters);
+	res_x = (float *)malloc(sizeof(float) * nClusters);
+	res_y = (float *)malloc(sizeof(float) * nClusters);
+	for(int i = 0; i < nClusters; i++) {
+		k_points[i] = 0;
+		sum_x[i] = 0;
+		sum_y[i] = 0;
     }
 
-    printf("N = %d, K = %d\n", N, K);
-    for (int i = 0; i < K; i++)
-        printf("Center: (%.3f, %.3f) : Size: %d\n", centroids[i].x, centroids[i].y, n_points[i]);
-    printf("Iterations: %d\n", iterations);
+	/* Distribute the work among all nodes. The data points itself will stay constant and
+	   not change for the duration of the algorithm. */
+	MPI_Scatter(data_x_points, (nPoints/nProcs), MPI_FLOAT, recv_x, (nPoints/nProcs), MPI_FLOAT, 0, MPI_COMM_WORLD);
+	MPI_Scatter(data_y_points, (nPoints/nProcs), MPI_FLOAT, recv_y, (nPoints/nProcs), MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-    free(points);
-    free(centroids);
-    free(new_x);
-    free(new_y);
-    free(n_points);
+	int it = 0;
+	while(it < MAX_ITERATIONS) {
+		// update k-means on all processes from root data
+		MPI_Bcast(k_means_x, nClusters, MPI_FLOAT, 0, MPI_COMM_WORLD);
+		MPI_Bcast(k_means_y, nClusters, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-    return 0;
+		// assign the data points to a cluster
+		assign_cluster(k_means_x, k_means_y, recv_x, recv_y, sum_x, sum_y, k_points);
+
+		// gather information
+		MPI_Reduce(sum_x, res_x, 4, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+		MPI_Reduce(sum_y, res_y, 4, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+		MPI_Reduce(k_points, recv_k_points, 4, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+		// let the root process recalculate k means
+		if(rank == 0) {
+			printf("--------------------------------------------------\n");
+            for(int i = 0; i < nClusters; i++) {
+				k_means_x[i] = res_x[i] / recv_k_points[i];
+				k_means_y[i] = res_y[i] / recv_k_points[i];
+				printf("Center #%d: (%.3f, %.3f) : Size: %d\n", i, k_means_x[i], k_means_y[i], recv_k_points[i]);
+		    }
+            printf("Iterations: %d\n", it+1);
+			printf("--------------------------------------------------\n");
+		}
+
+		for (int i = 0; i < nClusters; i++) {
+			sum_x[i] = 0;
+			sum_y[i] = 0;
+			k_points[i] = 0;
+		}
+
+		it++;
+	}
+
+	// deallocate memory and clean up
+	free(k_means_x);
+	free(k_means_y);
+	free(data_x_points);
+	free(data_y_points);
+	free(k_points);
+	free(recv_k_points);
+	free(recv_x);
+	free(recv_y);
+	free(sum_x);
+	free(sum_y);
+	free(res_x);
+	free(res_y);
+
+	//MPI_Barrier(MPI_COMM_WORLD);
+	MPI_Finalize();
 }
